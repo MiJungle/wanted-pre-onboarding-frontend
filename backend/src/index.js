f47b9c8609dcaf21d40f1d95db76bdf3b587dc7c
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-10-26 23:24:34 +0900

    add books api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
@@ -5,34 +7,27 @@ const app = new Koa();
 const router = new Router();
 const api = require('./api');
 
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser')
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGO_URI).then(
+  (response) =>{
+    console.log('Successfully connected to mongodb');
+  }
+).catch(e=> {
+  console.error(e);
+})
+
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser());
+
 router.use('/api',api.routes());
-
-
-
-// router.get('/', (ctx, next)=> {
-//   ctx.body = '홈';
-// })
-
-// router.get('/about', (ctx,next)=>{
-//   ctx.body = '소개';
-// })
-
-// router.get('/about/:name', (ctx, next)=> {
-//   const { name } = ctx.params;
-//   ctx.body = name + '의 소개'
-// })
-
-// router.get('/post', (ctx, next) => {
-//   const { id } = ctx.request.query;
-//   if(id){
-//     ctx.body = '포스트 #' + id;
-//   } else {
-//     ctx.body = '포스트 아이디가 없습니다.';
-//   }
-// });
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('server is listening to port 4000')
+app.listen(port, () => {
+  console.log(`server is listening to port ${port}`)
 })
