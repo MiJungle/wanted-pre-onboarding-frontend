2d1cc31339651da8d6d56831f09f2e35f79a5f5f
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-10-27 11:18:36 +0900

    add jwt token generateToken function & set cookies

--- backend/src/api/auth/index.js
+++ backend/src/api/auth/index.js
@@ -2,9 +2,13 @@ const Router = require('koa-router');
 const auth = new Router();
 const authCtrl = require('./auth.controller');
 
+// auth.get('/register/local', authCtrl.list);
+
+// auth.get('/register/local', authCtrl.localRegister);
 auth.post('/register/local', authCtrl.localRegister);
 auth.post('/login/local', authCtrl.localLogin);
 auth.get('/exists/:key(email|username)/:value', authCtrl.exists);
 auth.post('/logout', authCtrl.logout);
+auth.get('/check', authCtrl.check);
 
 module.exports = auth;
