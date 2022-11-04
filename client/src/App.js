d33ff2155b1735930b85328c642377a9172d43b5
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-11-04 20:09:41 +0900

    question pages and css styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import {BrowserRouter, Route, Routes, } from 'react-router-dom'
-import { Home, Auth } from './pages'
+import { Home, Auth, Question, Result } from './pages'
 import HeaderContainer from './containers/Base/HeaderContainer'
 import { Login, Register } from './containers/Auth'
 function App() {
   return (
   <Routes>
-    <Route element={<HeaderContainer/>}>
+    {/* <Route element={<HeaderContainer/>}> */}
       <Route path='/' element={<Home/>}></Route>
       <Route path='/auth' element={<Auth/>}></Route>
-    </Route>
+    {/* </Route> */}
+      <Route path='/question' element = {< Question/>}/>
+      <Route path="result" element={<Result/>}/> 
 
       <Route path="/auth/login" element={<Login/>}/>
-      <Route path="/auth/register" element={<Register/>}/>       
+      <Route path="/auth/register" element={<Register/>}/> 
+
         {/* <Route path="/signup" element={<SignUp/>}></Route>
         <Route path="/todo" element={<TodoList/>}></Route>
         <Route path="/" element={<SignIn/>}></Route> 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,85 @@
 import React from 'react';
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+
+const HomeForm = styled.div`
+    background-color: red;
+    margin: auto;
+    height: 200px;
+    width: 620px;
+    height: calc(100vh - 0.5rem);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-left: 0.25rem solid rgb(112, 93, 242);
+    border-right: 0.25rem solid rgb(112, 93, 242);
+    padding-bottom: 1rem;
+    flex: 1;
+    overflow-y: auto;
+`
+const TestHeading = styled.div`
+    font-size: 2rem;
+    font-stretch: normal;
+    font-style: normal;
+    text-align: center;
+    color: #222;
+    padding: 0 10px;
+    margin-top: 17vh;
+
+`
+const TestHeading2 = styled.div`
+    font-size: 20px;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -.5px;
+    margin: 30px 0;
+    justify-contents: center;
+
+`
+
+const TestStartBtn = styled.button`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 335px;
+    height: 100px;
+    margin: 30px 0;
+    border-radius: 20px;
+    background-color: #ff5100;
+    padding: 30px 30px;
+`
+const TestStartBtnVisits = styled.div`
+`
+const TestStartLogo = styled.button`
+`
+
+const TestFooterTitle = styled.div`
+    font-weight: 700;
+    margin-right: 8px;
+    margin-bottom: 4px;
+`
+
+const TestFooterEmail = styled.div`
+`
 
 function Home(){
     return (
+        <HomeForm>
+            <TestHeading>내 성격에 딱! 어울리는 TEA는?</TestHeading>
+            <TestHeading2>TEA로 보는 나의 성격, MB"TEA"I!</TestHeading2>
+            <Link to="/question">
+            <TestStartBtn >
+     
+                테스트 시작하기
+                <TestStartBtnVisits>현재 총 ... 명이 참여했어요</TestStartBtnVisits>
+            </TestStartBtn>
+            </Link>
 
-        <div>
-            Homeddd\
-            dfasdf
-            
-        </div>
+            <div>로고</div>
+            <div>문의</div>
+            <div>mijunglee1215@gmail.com</div>
+        </HomeForm>
     )
 }
 
